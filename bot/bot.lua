eadd07c4303650caa7ab3e5da5fc672714c0461b
mrmamad <tiavar66@gmail.com>
2016-11-25 21:43:20 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {168398326},
+    sudo_users = {1090049994},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
